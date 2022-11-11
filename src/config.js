cc906b7bae1243cf288275887d1a18b1e5917c18
Anderson Marques Sepúlveda <63729360+AndersonMS-NB@users.noreply.github.com>
2022-11-11 17:56:44 -0300

    Alt_no_Git

--- src/config.js
+++ src/config.js
@@ -3,11 +3,11 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à AMS Node API!
 
 module.exports = {
     // Conexão mongobd - banco de Dados
-    connectionString: 'mongodb+srv://ams:ams@amsdatabase.f94qdmw.mongodb.net/?retryWrites=true&w=majority',
+    connectionString: '',
 
     // Envo de E-mail
-    sendgridKey: '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',
+    sendgridKey: '',
 
     // Conexão Storage na Azure
-    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=amsnode;AccountKey=46y68lCticj9ZfA732S680yJTxFLNFxwlLQLA9BHvUz+92Ccucbpw//B4srYoOpIwXEXGNQ6bltX+AStCTa3ZA==;EndpointSuffix=core.windows.net'
+    containerConnectionString: ''
 }
